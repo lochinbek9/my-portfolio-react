10844567e73cb230bf0c5fd098204000996d570b
lochinbek9 <lochinbeksetor@gmail.com>
2025-05-26 23:08:38 +0500

    hero to'g'irlandi

--- src/components/Pages/Index.jsx
+++ src/components/Pages/Index.jsx
@@ -24,45 +24,59 @@ function Index() {
 
       <div className="container">
         <div className="hero position-absolute">
-            <div className="hero-content text-center">
-                <div className="section-title">
-                  <h5>
-                    <img src={titleShape} alt="title Shape" className='img-fluid me-2' />
-                    Welcome to Me Freelance Solutions
-                  </h5>
-                  <h1>Hire Top professionals to <br /> Transform your 
-                  <span> Ideas info reality</span> </h1>
-                </div>
-                <div className="hero-image mt-5">
-                  <img src={hero} alt="hero" className='img-fluid' />
-                  <div className="floting-img">
-                    <div className="floting-img-1">
-                      <img src={heroIcon1} className='img-fluid' alt="" />
-                    </div>
-                    <div className="floting-img-2">
-                      <img src={heroIcon2} className='img-fluid' alt="" />
-                    </div>
-                  </div>
-                  <div className="floting-text">
-                      <span>Web Developer</span>
-                      <span>Graphic Design</span>
-                  </div>
-                </div>
+          <div className="hero-content text-center">
+            <div className="section-title">
+              <h5>
+                <img src={titleShape} alt="title Shape" className='img-fluid me-2' />
+                Welcome to Me Freelance Solutions
+              </h5>
+              <h1>Hire Top professionals to <br /> Transform your
+                <span> Ideas info reality</span> </h1>
             </div>
+            <div className="hero-image mt-5">
+              <img src={hero} alt="hero" className='img-fluid' />
+              <div className="floting-img">
+                <div className="floting-img-1">
+                  <img src={heroIcon1} className='img-fluid' alt="" />
+                </div>
+                <div className="floting-img-2">
+                  <img src={heroIcon2} className='img-fluid' alt="" />
+                </div>
+              </div>
+              <div className="floting-text">
+                <span>Web Developer</span>
+                <span>Graphic Design</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
       {/* MARQUE LOGO */}
 
-      <div className="hero-marquee position-absolute">
-          <marquee behavior="alternate" direction="">
-            <img src={logo1} alt="logo1" className='img-fluid ms-5' />
-            <img src={logo2} alt="logo1" className='img-fluid ms-5' />
-            <img src={logo3} alt="logo1" className='img-fluid ms-5' />
-            <img src={logo4} alt="logo1" className='img-fluid ms-5' />
-            <img src={logo5} alt="logo1" className='img-fluid ms-5' />
-            <img src={logo6} alt="logo1" className='img-fluid ms-5' />
-          </marquee>
+      <div className="hero-marquee">
+        <marquee behavior="alternate" direction="">
+          <img src={logo1} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo2} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo3} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo4} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo5} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo6} alt="logo1" className='img-fluid ms-5' />
+
+          <img src={logo1} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo2} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo3} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo4} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo5} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo6} alt="logo1" className='img-fluid ms-5' />
+
+          <img src={logo1} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo2} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo3} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo4} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo5} alt="logo1" className='img-fluid ms-5' />
+          <img src={logo6} alt="logo1" className='img-fluid ms-5' />
+        </marquee>
       </div>
     </>
   )
