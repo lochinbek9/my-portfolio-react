c94731b0f06adce3ac237f786a89795a7fa5dcb7
lochinbek9 <ldehkonov9@gmail.com>
2025-05-29 16:56:45 +0500

    about us qo'shildi

--- src/components/Pages/Index.jsx
+++ src/components/Pages/Index.jsx
@@ -21,6 +21,15 @@ import aboutIcon2 from "./../../assets/img/icon-about-agency-2.svg"
 import aboutClient from "./../../assets/img/author-image.jpg"
 import btnArrow from "./../../assets/img/btn-arrow.svg"
 
+import service1 from "./../../assets/img/service-image-1.jpg"
+import service2 from "./../../assets/img/service-image-2.jpg"
+import service3 from "./../../assets/img/service-image-3.jpg"
+
+import serviceIcon1 from "./../../assets/img/icon-service-item-1.svg"
+import serviceIcon2 from "./../../assets/img/icon-service-item-2.svg"
+import serviceIcon3 from "./../../assets/img/icon-service-item-3.svg"
+
+
 function Index() {
   return (
     <>
@@ -28,11 +37,11 @@ function Index() {
         <video muted autoPlay loop>
           <source src={videoBg} />
         </video>
-      </div> 
+      </div>
 
-   <section className='hero'>
-       <div className="container">
-       
+      <section className='hero'>
+        <div className="container">
+
           <div className="hero-content text-center">
             <div className="section-title">
               <h5>
@@ -43,13 +52,13 @@ function Index() {
                 <span> Ideas info reality</span> </h1>
             </div>
           </div>
-      
-      </div>
-   </section>
+
+        </div>
+      </section>
 
       {/* MARQUE LOGO */}
 
-       <div className="hero-marquee">
+      <div className="hero-marquee">
         <marquee behavior="alternate" direction="">
           <img src={logo1} alt="logo1" className='img-fluid ms-5' />
           <img src={logo2} alt="logo1" className='img-fluid ms-5' />
@@ -72,26 +81,89 @@ function Index() {
           <img src={logo5} alt="logo1" className='img-fluid ms-5' />
           <img src={logo6} alt="logo1" className='img-fluid ms-5' />
         </marquee>
-      </div> 
+      </div>
 
       {/* ABOUT-US */}
 
-      <div className="large-section pt-5 about-us">
-        <div className="container my-5 py-5">
+      <div className="large-section position-relative pt-5 about-us">
+        <div className="container about-wrapper my-5 py-5">
           <div className="row">
-              <div className="col-lg-6">
+            <div className="col-lg-6">
+              <div className="d-flex gap-3">
                 <img src={about1} className='img-fluid rounded' alt="" />
-                <div className="d-flex flex-column gap-4">
+                <div className="d-flex flex-column gap-4 about-sec-image">
                   <img src={aboutCircle} className='img-fluid about-small-img1' alt="" />
                   <img src={about2} className='img-fluid rounded about-small-img2' alt="" />
                 </div>
               </div>
-              <div className="col-lg-6"></div>
+            </div>
+            <div className="col-lg-6">
+              <div className="about-content">
+                <div className="section-title">
+                  <h5>
+                    <img src={titleShape} className='img-fluid me-2' alt="" />
+                    welcome to our freelance solutions
+                  </h5>
+                  <h2>Driving <span>growth</span> though smarter SEO</h2>
+                </div>
+                <div className="d-flex about-info-text justify-content-between align-items-center py-5" style={{ borderBottom: "1px solid #3d3f3c" }}>
+                  <h6>
+                    <img src={titleShape} className='img-fluid me-3' alt="" />
+                    Client-Centric Approach
+                  </h6>
+                  <h6>
+                    <img src={titleShape} className='img-fluid me-3' alt="" />
+                    Seamless Communication
+                  </h6>
+                </div>
+                <div className="d-flex about-info-text justify-content-between align-items-center py-5" style={{ borderBottom: "1px solid #3d3f3c" }}>
+                  <div className="d-flex">
+                    <img src={aboutIcon1} className='img-fluid' alt="" />
+                    <h4>Deticated to Client Success</h4>
+                  </div>
+                  <div className="d-flex">
+                    <img src={aboutIcon2} className='img-fluid' alt="" />
+                    <h4>Affirdable Service With Quality</h4>
+                  </div>
+                </div>
+                <div className="d-flex justify-content-between align-items-center py-5">
+                  <div className="about-client d-flex gap-3">
+                    <div className="about-client-img">
+                      <img src={aboutClient} className='img-fluid rounded-circle' alt="" />
+                    </div>
+                    <div className="about-client-info">
+                      <h5 className='fw-bold'>sarah mitchell</h5>
+                      <h5 className='m-0'>marketing director</h5>
+                    </div>
+                  </div>
+                  <div className="btn-1">
+                    <button className="btn">More About</button>
+                    <img src={btnArrow} alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div> 
-
-
+      </div>
+      <div className="service section my-5 py-5">
+        <div className="container my-5 py-5">
+          <div className="row g-4">
+            <div className="col-lg-4">
+              <div className="ser-card h-100">
+                  <div className="section-image">
+                      <img src={service1} className='card-img-top' alt="" />
+                  </div>
+                  <div className="ser-card-body">
+                      <div className="ser-icon mb-3">
+                          <img src="" alt="" />
+                      </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
